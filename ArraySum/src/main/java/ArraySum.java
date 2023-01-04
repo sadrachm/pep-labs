1a865ac368f3e4114a62672f0738b855b39996ea
Sadrach Martinez <sadrachmart@gmail.com>
2023-01-04 01:39:32 +0000

    Saved progress on ArraySum 2023-01-04 01:39:32 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,10 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int sum = 0;
+        for (int x : arr) {
+            sum+= x;
+        }
+        return sum;
     }
 }
