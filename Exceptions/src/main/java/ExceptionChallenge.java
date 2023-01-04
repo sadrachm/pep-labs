ae38bc172f7fa9677570b2aeba191d159c09815a
Sadrach Martinez <sadrachmart@gmail.com>
2023-01-04 01:06:34 +0000

    Saved progress on Exceptions 2023-01-04 01:06:34 +0000

--- Exceptions/src/main/java/ExceptionChallenge.java
+++ Exceptions/src/main/java/ExceptionChallenge.java
@@ -17,5 +17,6 @@ public class ExceptionChallenge {
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
     public void mustThrow(){
+        int x = 9 / 0;
     }
 }
