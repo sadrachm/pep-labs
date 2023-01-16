d4110ca8b31643231e6515815aa4ed638788304f
Sadrach Martinez <sadrachmart@gmail.com>
2023-01-16 01:26:00 +0000

    Saved progress on Contains 2023-01-16 01:26:00 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for (int a : arr) {
+            if (a==target) {
+                return true;
+            }
+        }
         return false;
     }
 }
