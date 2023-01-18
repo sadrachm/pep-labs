8cb6318f335a72079845a0dc99efc4e4c8b9de9e
Sadrach Martinez <sadrachmart@gmail.com>
2023-01-18 19:53:49 +0000

    Saved progress on RequestBody 2023-01-18 19:53:49 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -18,6 +20,12 @@ public class JavalinSingleton {
          */
         app.post("/problem1", ctx -> {
                 //implement logic here
+                String body = ctx.body();
+                ObjectMapper obj = new ObjectMapper();
+                Song song = obj.readValue(body, Song.class);
+                // ctx.contentType("application/json");
+                ctx.result(song.getArtistName());
+
         });
 
         /**
@@ -29,6 +37,13 @@ public class JavalinSingleton {
          */
         app.post("/problem2", ctx -> {
                //implement logic here
+               String body = ctx.body();
+               ObjectMapper obj = new ObjectMapper();
+               Song song = obj.readValue(body, Song.class);
+               ctx.contentType("application/json");
+                song.setArtistName("Beatles");
+                String json = obj.writeValueAsString(song);
+                ctx.result(json);
         });
 
 
