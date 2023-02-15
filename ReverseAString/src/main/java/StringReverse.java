6d9421379fda0a8a3a1356508f94095812efec30
Sadrach Martinez <sadrachmart@gmail.com>
2023-02-15 20:54:29 +0000

    Saved progress on ReverseAString 2023-02-15 20:54:29 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -13,6 +13,10 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        String x = "";
+        for (int i =0; i < str.length(); i++) {
+            x = str.charAt(i) + x;
+        }
+        return x;
     }
 }
